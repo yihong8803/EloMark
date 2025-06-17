8ab174009a55273b6d7f507ffdb9ef8f192dfca8
yihong8803 <yihong8803@gmail.com>
2025-06-17 19:39:58 +0800

    Match color

--- elomark/lib/color.dart
+++ elomark/lib/color.dart
@@ -5,7 +5,7 @@ const Color whiteGrey = Color.fromARGB(255, 213, 213, 213);
 const Color primary = Color.fromARGB(255, 221, 212, 143);
 const Color primaryText = Color.fromARGB(255, 112, 64, 18);
 const Color button = Color.fromARGB(255, 107, 140, 33);
-const Color primaryColor_darkGreen = Color.fromARGB(255, 74, 115, 55);
+const Color primaryColor_darkGreen = Color.fromARGB(255, 73, 115, 55);
 
 const TextName =
     TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.w400);
